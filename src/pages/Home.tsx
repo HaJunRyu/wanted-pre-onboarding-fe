91b88172bcdc672bd321cc1f8dc46cbe442cfaa6
HaJunRyu <hjr4557@naver.com>
2022-08-24 15:00:53 +0900

    Env: emotion 의존성을 추가하고 설정한다

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Test = styled.div`
+  color: red;
+`;
 
 function Home() {
-  return <div>Home</div>;
+  return <Test>Home</Test>;
 }
 
 export default Home;
